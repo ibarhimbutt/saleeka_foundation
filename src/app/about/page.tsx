468b20db0af20a9c273713474b5e28f9972fa162
Najam Rashid <najam@linxtel.com>
2025-06-06 01:24:24 +0000

    use this image for mission page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -22,7 +22,7 @@ export default function AboutPage() {
               width={600}
               height={450}
               className="w-full h-auto object-cover"
-              data-ai-hint="collaboration team"
+              data-ai-hint="mission icons"
             />
           </div>
           <div>
